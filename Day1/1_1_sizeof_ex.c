32bcb89fde18adf2a4f87c7b8905bee90ade589b
Bala Priya <47279635+balapriyac@users.noreply.github.com>
2021-07-31 15:24:45 +0530

    Create 1_1_sizeof_ex.c

---
+++ Day1/1_1_sizeof_ex.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("Size of int: %ld bytes\n",sizeof(int));
+    printf("Size of float: %ld bytes\n",sizeof(float));
+    printf("Size of double: %ld bytes\n",sizeof(double));
+    printf("Size of char: %ld bytes\n",sizeof(char));
+    
+    return 0;
+}
