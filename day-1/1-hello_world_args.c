4b7b1505ece7fc372804e3a640e1a7dda93ace1f
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2021-10-19 17:47:12 +0530

    Update 1-hello_world_args.c

--- day-1/1-hello_world_args.c
+++ day-1/1-hello_world_args.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+/* This program displays all arguments 
+that are passed to it */
+
+int main(int argc, char **argv) //int main(int argc, char *argv[]) would work fine too
+{ 
+	int i;
+	for (i = 0; i < argc; i++) 
+  {
+		printf("argc = %d arg %d is %s\n", argc, i, argv[i]);	// prints arg at index i
+	}
+	return 0;
+}
