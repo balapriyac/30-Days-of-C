43b57a1ba7a86cade2efda576299d1f0af9dcd66
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2021-10-20 11:53:29 +0530

    Update 1-calc_total.c

--- day-2/1-calc_total.c
+++ day-2/1-calc_total.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+
+int main() 
+{
+	double sub_total;
+	double tax;
+	double grand_total;
+	double tax_rate;
+
+	tax_rate = 0.25;
+	sub_total = 250;
+	tax = sub_total * tax_rate;
+	grand_total = sub_total + tax;
+
+	printf("The tax on %.2f is %.2f, so the grand total is %.2f.\n", sub_total, tax, grand_total);
+	return 0;
+}
 
