5b4d20fe20b19f29ff18be3d9c595ae5e8d0ac3d
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2021-10-21 16:28:37 +0530

    Update 2-compound-assignment.c

--- day-3/2-compound-assignment.c
+++ day-3/2-compound-assignment.c
@@ -1,1 +1,46 @@
+#include <stdio.h>
+
+int main() 
+{
+
+	int a = 10;
+	int b = 5;
+
+
+	printf("\nExamples of compound assignment operators:\n");
+	a = a + b;
+	printf("\na + b  : %d\n", a);
+
+	a += b;
+	printf("a += b  : %d\n", a);
+
+	a = a - b;
+	printf("a - b  : %d\n", a);
+
+	a -= b;
+	printf("a -= b  : %d\n", a);
+
+	a = a * b;
+	printf("a * b  : %d\n", a);
+
+	a *= b;
+	printf("a *= b  : %d\n", a);
+
+	a = a / b;
+	printf("a/b  : %d\n", a);
+
+	a /= b;
+	printf("a /= b  : %d\n", a);
+  
+	a = 10;
+	a++;
+	printf("a++ : %d\n", a);
+  
+	a = 10;
+	a--;
+	printf("a-- : %d\n", a);
+
+
+	return(0);
+}
 
