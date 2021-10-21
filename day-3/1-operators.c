2fe818cad7a5f7ee3aeeb41bbab8cb099efca1b9
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2021-10-21 16:26:29 +0530

    Update 1-operators.c

--- day-3/1-operators.c
+++ day-3/1-operators.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main() 
+{
+  int marks = 90; //assignment
+
+	if (marks == 100) //equality
+	{
+	    printf("Perfect score!\n");
+	} 
+	else 
+	{
+	    printf("Great effort!\n");
+	}
+  
+  return 0;
+}
