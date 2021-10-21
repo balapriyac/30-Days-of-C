148bb37813c4291320ece764460ce2306b4f2aca
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2021-10-21 16:30:52 +0530

    Update 3-pre-post-increment.c

--- day-3/3-pre-post-increment.c
+++ day-3/3-pre-post-increment.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+void pre_and_post_inc() 
+{
+    int num1, num2;
+
+    num1 = 10;
+    num2 = num1++; // num2 = 10, num1 = 11
+    printf("\nnum2 = num1++; so num2 = %d and num1 = %d", num2, num1);
+
+    num1 = 10;
+    num2 = ++num1; // num2 = 11, num1 = 11
+    printf("\nnum2 = ++num1; so num2 = %d and num1 = %d", num2, num1);
+}
+
+int main()
+{
+    pre_and_post_inc();
+    return 0;
+}
