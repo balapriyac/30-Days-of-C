de6eeccf82f486b24773ab130d70dc6fffe19035
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2021-10-21 16:44:18 +0530

    Update 4-logical-ops.c

--- day-3/4-logical-ops.c
+++ day-3/4-logical-ops.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+int main() 
+{
+    int hours; //num_hrs per week
+    int years; //num_years of experience
+    int num_lang; //num_langauges known
+
+    hours = 40;
+    years = 2;
+    num_lang = 4;
+
+
+    if ((hours >= 40) && (years >= 2) && (num_lang > 3)) 
+    {
+      printf("Be sure to have a healthy work-life balance\n");
+    } 
+    else 
+    {
+      printf("Hope you're enjoying your work!\n");
+    }
+
+    return 0;
+}
