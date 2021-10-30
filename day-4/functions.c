d3e9318dbd4bf1a389f6038b4cc7a940d0c7366c
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2021-10-30 13:38:01 +0530

    Update functions.c

--- day-4/functions.c
+++ day-4/functions.c
@@ -1,1 +1,39 @@
+#include <stdio.h>
+
+void sayHello() 
+{
+	printf("Hello\n");
+}
+
+void greet(char my_name[]) 
+{
+	printf("Hello %s\n", my_name);
+}
+
+int add(int num1, int num2) 
+{
+	int num3;
+	num3 = num1 + num2;
+	return num3;
+}
+
+
+int main() 
+{
+	double result;
+	int n1;
+	int n2;
+	int total;
+
+	n1 = 10;
+	n2 = 30;
+
+	sayHello();
+	greet("Bala!");
+
+	total = add(n1, n2);
+	printf("%d + %d = %d\n", n1, n2, total);
+	
+	return 0;
+}
 
