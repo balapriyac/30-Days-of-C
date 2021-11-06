091e264b5049a2f697fffb52d21f4d4341905b58
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2021-11-06 14:31:49 +0530

    Update arrays_loops.c

--- day-4/arrays_loops.c
+++ day-4/arrays_loops.c
@@ -1,1 +1,47 @@
+#include <stdio.h>
+
+int array1[5];
+
+
+
+int main() {
+	int i;
+	for (i = 0; i < 5; i++) {
+		array1[i] = i + 1; 
+	}
+
+	printf("--- for loop ---\n");
+	for (i = 0; i < 5; i++) {
+		printf("%d\n", array1[i]);
+	}
+
+	printf("--- while loop ---\n");
+	i = 0;
+	while (i < 5) {
+		printf("%d\n", array1[i]);
+		i++;
+	}
+
+	printf("--- do..while loop ---\n");
+	i = 0;
+	do {
+		printf("%d\n", array1[i]);
+		i++;
+	} while (i < 5);
+
+	printf("--- while loop (i = 5) ---\n");
+	i = 5;
+	while (i < 5) {
+		printf("%d\n", intarray[i]);
+		i++;
+	}
+
+	printf("--- do..while loop (i = 5) ---\n");
+	i = 5;
+	do {
+		printf("%d\n", intarray[i]);
+		i++;
+	} while (i < 5);
+	return 0;
+}
 
