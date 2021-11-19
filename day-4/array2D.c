3c99dcf4a156d6333f574175ae07d3bf997801f7
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2021-11-19 18:43:36 +0530

    Update array2D.c

--- day-4/array2D.c
+++ day-4/array2D.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int grid[3][4] = {
+	{ 1,   2,   3,   4 },
+	{ 7,   8,   9,   10 },
+	{ 12,  13,  14,  15 }
+};
+
+int main() {
+	int row;
+	int column;
+	for (row = 0; row < 3; row++) {
+		printf("--- row %d --- \n", row);
+		for (column = 0; column < 4; column++) {
+			printf("column[%d], value=%d\n", column, grid[row][column]);
+		}
+	}
+	return 0;
+}
