e97e61d0bf652c36cc4be16ae47bf619445d37ae
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2021-11-19 19:01:27 +0530

    Update declare_str.c

--- day-5/declare_str.c
+++ day-5/declare_str.c
@@ -1,1 +1,9 @@
+#include <stdio.h>
 
+int main() {
+	char str1[] = "Hello";
+	char *str2 = "Bye";
+	printf("%d %d %s\n", &str1, str1, str1);
+	printf("%d %d %s\n", &str2, str2, str2);
+	return 0;
+}
