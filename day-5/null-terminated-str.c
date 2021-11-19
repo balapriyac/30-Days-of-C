18e36def1d8c211ce212eba6db910f223e4cb770
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2021-11-19 19:06:34 +0530

    Update null-terminated-str.c

--- day-5/null-terminated-str.c
+++ day-5/null-terminated-str.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+char str1[] = "Hello"; //'\0' is auto-appended
+char str2[] = "Hello\0 world"; //'\0' terminates the string
+
+void showstring(char *str) {
+	int i;
+	int len = strlen(str);
+	printf("Length of %s is %d.\n", str, len);
+	for (i = 0; i <= len; i++) {
+		if (str[i] == '\0') {
+			printf("char[%d] = NULL\n", i);
+		} else {
+			printf("char[%d] = %c\n", i, str[i]);
+		}
+	}
+}
+
+int main() {
+	showstring(str1);
+	showstring(str2);
+	printf("%s\n", str1);
+	printf("%s\n", str2);
+	return 0;
+}
