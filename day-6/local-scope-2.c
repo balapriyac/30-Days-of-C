8505cc89f6cbf1c2ea2a9e8decb195f636a1555d
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2021-11-20 10:08:40 +0530

    Update local-scope-2.c

--- day-6/local-scope-2.c
+++ day-6/local-scope-2.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+
+int main() 
+{
+    int my_num = 7;
+    {
+        int new_num = 10;
+    } 
+    printf("new_num is %d",new_num); //this is line 9; new_num can't be accessed here
+    return 0;
+}
+
 
