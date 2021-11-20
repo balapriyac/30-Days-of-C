0c0838b5b22c496e6d85a39a5ea2f768f1e5346e
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2021-11-20 10:07:49 +0530

    Update local-scope-1.c

--- day-6/local-scope-1.c
+++ day-6/local-scope-1.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+
+int main() 
+{
+    int my_num = 7;
+    {
+        //add 10 my_num
+        my_num = my_num +10;
+        //or my_num +=10 - more succinctly
+        printf("my_num is %d",my_num);
+    }
+    
+    return 0;
+}
 
