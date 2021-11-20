8e658db7d685c3824b9c8bf341b6d5446b6489bc
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2021-11-20 10:40:24 +0530

    Update string-ex.c

--- day-5/string-ex.c
+++ day-5/string-ex.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	char str1[] = "Hello";
+	char *str2 = "Hello";
+	char str3[6];
+	
+	str3[0] = 'H';
+	str3[1] = 'e';
+	str3[2] = 'l';
+	str3[3] = 'l';
+	str3[4] = 'o';
+	str3[5] = '\0';
+	
+	printf("%d %s\n", str1, str1);
+	printf("%d %s\n", str2, str2);
+	printf("%d %s\n", str3, str3);
+	
+	printf("%d, %d, %d, %d, %d\n", &str3[0], &str3[1], &str3[2], &str3[3], &str3[4]);
+	printf("%d, %d, %d, %d, %d\n", str3, str3+1, str3+2, str3+3, str3+4);
+	
+	str2 = "Goodbye";
+	printf("%s", str2);
+	
+	
+	
+	return 0;
+}
 
