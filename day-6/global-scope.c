998076b4e00609019de39a907a66ebabd1d374f9
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2021-11-20 10:10:16 +0530

    Update global-scope.c

--- day-6/global-scope.c
+++ day-6/global-scope.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+int my_num = 7;
 
+int main()
+{
+    printf("my_num can be accessed from main() and its value is %d\n",my_num);
+    //call my_func
+    my_func();
+    return 0;
+}
+
+void my_func()
+{
+  printf("my_num can be accessed from my_func() as well and its value is %d\n",my_num);
+}
